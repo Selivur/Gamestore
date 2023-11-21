421f07701c01dec66bfaedbe7f51591dcc8b6782
Illia Semenenko <illia_semenenko@epam.com>
2023-11-21 20:45:14 +0200

    Added GenreServices

---
+++ Gamestore/Gamestore.Api/Services/GenreService.cs
@@ -0,0 +1,110 @@
+﻿using Gamestore.Api.Models.DTO.GenreDTO;
+using Gamestore.Api.Services.Interfaces;
+using Gamestore.Database.Entities;
+using Gamestore.Database.Repositories.Interfaces;
+
+namespace Gamestore.Api.Services;
+
+/// <summary>
+/// Service for managing genres.
+/// </summary>
+public class GenreService : IGenreService
+{
+    private readonly IGenreRepository _repository;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GenreService"/> class.
+    /// </summary>
+    /// <param name="genreRepository">The repository for genres.</param>
+    public GenreService(IGenreRepository genreRepository)
+    {
+        _repository = genreRepository;
+    }
+
+    /// <inheritdoc/>
+    public async Task<IEnumerable<Genre>> GetAllGenresAsync()
+    {
+        return await _repository.GetAllAsync();
+    }
+
+    /// <inheritdoc/>
+    public async Task AddGenreAsync(GenreRequest genre)
+    {
+        var existingGame = await _repository.GetByNameAsync(genre.Name);
+
+        if (existingGame != null)
+        {
+            throw new InvalidOperationException("Genre name must be unique");
+        }
+
+        Genre newGenre = new()
+        {
+            Name = genre.Name,
+            ParentId = genre.ParentId,
+        };
+
+        await _repository.AddAsync(newGenre);
+    }
+
+    /// <inheritdoc/>
+    public async Task<GenreResponse?> GetGenreByNameAsync(string name)
+    {
+        var genre = await _repository.GetByNameAsync(name) ?? throw new KeyNotFoundException("Genre not found");
+
+        return GenreResponse.FromGenre(genre);
+    }
+
+    /// <inheritdoc/>
+    public async Task<GenreResponse?> GetGenreByIdAsync(int id)
+    {
+        var genre = await _repository.GetByIdAsync(id) ?? throw new KeyNotFoundException("Genre not found");
+
+        return GenreResponse.FromGenre(genre);
+    }
+
+    /// <inheritdoc/>
+    public async Task UpdateGenreAsync(int id, GenreRequest genre)
+    {
+        var existingGenre = await _repository.GetByIdAsync(id) ?? throw new KeyNotFoundException("Can't find the Genre with this id");
+
+        existingGenre.Name = genre.Name;
+        existingGenre.ParentId = genre.ParentId;
+
+        await _repository.UpdateAsync(existingGenre);
+    }
+
+    /// <inheritdoc/>
+    public async Task RemoveGenreAsync(string name)
+    {
+        await _repository.RemoveAsync(name);
+    }
+
+    /// <inheritdoc/>
+    public async Task InitializePredefinedGenresAsync()
+    {
+        var genres = new List<Genre>
+        {
+            new() { Name = "Strategy" },
+            new() { Name = "RTS", ParentId = 1 },
+            new() { Name = "TBS", ParentId = 1 },
+            new() { Name = "RPG" },
+            new() { Name = "Sports" },
+            new() { Name = "Races" },
+            new() { Name = "Rally", ParentId = 6 },
+            new() { Name = "Arcade", ParentId = 6 },
+            new() { Name = "Formula", ParentId = 6 },
+            new() { Name = "Off-road", ParentId = 6 },
+            new() { Name = "Action" },
+            new() { Name = "FPS", ParentId = 11 },
+            new() { Name = "TPS", ParentId = 11 },
+            new() { Name = "Adventure" },
+            new() { Name = "Puzzle & Skill" },
+            new() { Name = "Misc." },
+        };
+
+        foreach (var genre in genres)
+        {
+            await _repository.AddAsync(genre);
+        }
+    }
+}
---
+++ Gamestore/Gamestore.Api/Services/Interfaces/IGenreService.cs
@@ -0,0 +1,58 @@
+﻿using Gamestore.Api.Models.DTO.GenreDTO;
+using Gamestore.Database.Entities;
+
+namespace Gamestore.Api.Services.Interfaces;
+
+/// <summary>
+/// Service for managing genres.
+/// </summary>
+public interface IGenreService
+{
+    /// <summary>
+    /// Gets all genres.
+    /// </summary>
+    /// <returns>The collection of all genres.</returns>
+    Task<IEnumerable<Genre>> GetAllGenresAsync();
+
+    /// <summary>
+    /// Adds a new genre.
+    /// </summary>
+    /// <param name="genre">The data for the new genre.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task AddGenreAsync(GenreRequest genre);
+
+    /// <summary>
+    /// Gets a genre by its name.
+    /// </summary>
+    /// <param name="name">The name of the genre.</param>
+    /// <returns>The detailed information about the genre.</returns>
+    Task<GenreResponse?> GetGenreByNameAsync(string name);
+
+    /// <summary>
+    /// Gets a genre by its name.
+    /// </summary>
+    /// <param name="id">The name of the genre.</param>
+    /// <returns>The detailed information about the genre.</returns>
+    Task<GenreResponse?> GetGenreByIdAsync(int id);
+
+    /// <summary>
+    /// Updates a genre by its identifier.
+    /// </summary>
+    /// <param name="id">The identifier of the genre to be updated.</param>
+    /// <param name="genre">The updated data for the genre.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task UpdateGenreAsync(int id, GenreRequest genre);
+
+    /// <summary>
+    /// Removes a genre by its name.
+    /// </summary>
+    /// <param name="name">The name of the genre to be removed.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task RemoveGenreAsync(string name);
+
+    /// <summary>
+    /// Initializes predefined genres in the database.
+    /// </summary>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task InitializePredefinedGenresAsync();
+}
