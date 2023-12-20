32fd835cf089fdd035fc19cfe7c6c01fa67e9d99
Illia Semenenko <illia_semenenko@epam.com>
2023-12-20 16:43:14 +0200

    Added to DAL Order CRUD

---
+++ Gamestore/Gamestore.DataBase/Repositories/Interfaces/IOrderRepository.cs
@@ -0,0 +1,44 @@
+﻿using Gamestore.Database.Entities;
+
+namespace Gamestore.Database.Repositories.Interfaces;
+
+/// <summary>
+/// Represents an interface for interacting with a order repository.
+/// </summary>
+public interface IOrderRepository
+{
+    /// <summary>
+    /// Retrieves a order by its unique identifier asynchronously.
+    /// </summary>
+    /// <param name="id">The unique identifier of the order to retrieve.</param>
+    /// <returns>An asynchronous task that returns the retrieved order.</returns>
+    Task<Order?> GetByIdAsync(int id);
+
+    /// <summary>
+    /// Retrieves all games in the repository asynchronously.
+    /// </summary>
+    /// <returns>An asynchronous task that returns a collection of all games in the repository.</returns>
+    Task<IEnumerable<Order>> GetAllAsync();
+
+    /// <summary>
+    /// Adds a new order to the repository asynchronously.
+    /// </summary>
+    /// <param name="order">The order to add to the repository.</param>
+    /// <returns>An asynchronous task representing the operation's completion or throwing an error when failed to add the order.</returns>
+    Task AddAsync(Order order);
+
+    /// <summary>
+    /// Updates an existing order in the repository asynchronously.
+    /// </summary>
+    /// <param name="order">The order to update in the repository.</param>
+    /// <returns>An asynchronous task representing the operation's completion or throwing an error when failed to update the order.</returns>
+    Task UpdateAsync(Order order);
+
+    /// <summary>
+    /// Removes a order from the repository by its unique identifier asynchronously.
+    /// </summary>
+    /// <param name="id">The id of the order to remove.</param>
+    /// <returns>An asynchronous task representing the operation's completion or throwing an error when failed to remove the order.</returns>
+    /// <exception cref="ArgumentException">Thrown if no order is found with the specified company name.</exception>
+    Task RemoveAsync(int id);
+}
---
+++ Gamestore/Gamestore.DataBase/Repositories/OrderRepository.cs
@@ -0,0 +1,78 @@
+﻿using Gamestore.Database.Dbcontext;
+using Gamestore.Database.Entities;
+using Gamestore.Database.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gamestore.Database.Repositories;
+
+/// <summary>
+/// Implementation of the <see cref="IOrderRepository"/> interface.
+/// Provides data access functionality for managing order entities.
+/// </summary>
+public class OrderRepository : IOrderRepository
+{
+    private readonly GamestoreContext _context;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OrderRepository"/> class.
+    /// </summary>
+    /// <param name="context">The database context for interacting with the underlying data store.</param>
+    public OrderRepository(GamestoreContext context)
+    {
+        _context = context;
+    }
+
+    /// <inheritdoc />
+    public async Task<Order?> GetByIdAsync(int id)
+    {
+        return await _context.Orders.FindAsync(id);
+    }
+
+    /// <inheritdoc />
+    public async Task<IEnumerable<Order>> GetAllAsync()
+    {
+        return await _context.Orders.ToListAsync();
+    }
+
+    /// <inheritdoc />
+    public async Task AddAsync(Order order)
+    {
+        _context.Orders.Add(order);
+        await SaveChangesAsync("Error when adding the order to the database.");
+    }
+
+    /// <inheritdoc />
+    public async Task UpdateAsync(Order order)
+    {
+        _context.Entry(order).State = EntityState.Modified;
+
+        await SaveChangesAsync("Error when updating the order in the database.");
+    }
+
+    /// <inheritdoc />
+    public async Task RemoveAsync(int id)
+    {
+        var order = await _context.Orders.SingleOrDefaultAsync(g => g.Id.Equals(id))
+            ?? throw new ArgumentException($"No order found with the id '{id}'.", nameof(id));
+
+        _context.Orders.Remove(order);
+
+        await SaveChangesAsync("Error when deleting the order from the database.");
+    }
+
+    /// <summary>
+    /// Asynchronously saves changes to the database context and throws a <see cref="DbUpdateException"/>
+    /// with the specified error message if no changes were saved.
+    /// </summary>
+    /// <param name="errorMessage">The error message to be included in the exception if no changes were saved.</param>
+    /// <returns>An asynchronous task representing the operation's completion or throwing a <see cref="DbUpdateException"/>.</returns>
+    private async Task SaveChangesAsync(string errorMessage)
+    {
+        var saved = await _context.SaveChangesAsync();
+
+        if (saved == 0)
+        {
+            throw new DbUpdateException(errorMessage);
+        }
+    }
+}
