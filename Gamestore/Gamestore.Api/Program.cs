40b3f098e588e9207e1d72c01e0b7fd2731154ec
Illia Semenenko <illia_semenenko@epam.com>
2024-01-17 20:26:24 +0200

    Added visible comments in swagger

--- Gamestore/Gamestore.Api/Program.cs
+++ Gamestore/Gamestore.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Gamestore.Api.Filters;
 using Gamestore.Api.Middleware;
 using Gamestore.Api.Services;
@@ -34,7 +35,12 @@ builder.Services.AddControllers(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
