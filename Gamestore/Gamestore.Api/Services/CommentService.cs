2236bbcead38a5ba2c6e62d9de856080dced8c75
Illia Semenenko <illia_semenenko@epam.com>
2024-02-13 01:12:37 +0200

    Added CommentService

---
+++ Gamestore/Gamestore.Api/Services/CommentService.cs
@@ -0,0 +1,64 @@
+﻿using Gamestore.Api.Models.DTO.CommentDTO;
+using Gamestore.Api.Services.Interfaces;
+using Gamestore.Database.Entities;
+using Gamestore.Database.Repositories.Interfaces;
+
+namespace Gamestore.Api.Services;
+
+public class CommentService : ICommentService
+{
+    private readonly ICommentRepository _commentRepository;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CommentService"/> class.
+    /// </summary>
+    /// <param name="commentRepository">The comment repository providing data access for the service.</param>
+    public CommentService(ICommentRepository commentRepository)
+    {
+        _commentRepository = commentRepository;
+    }
+
+    /// <inheritdoc/>
+    public async Task AddCommentAsync(CommentRequest commentRequest, Game game)
+    {
+        // TODO add check ref comment id != id
+        // TODO add reply(external method)
+        Comment comment = new()
+        {
+            Name = commentRequest.Name,
+            Body = commentRequest.Body,
+            Game = game,
+        };
+
+        await _commentRepository.AddAsync(comment);
+    }
+
+    /// <inheritdoc/>
+    public Task<CommentResponse?> GetCommentByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public Task UpdateCommentAsync(CommentRequest commentRequest)
+    {
+        // TODO add check ref comment id != id
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public Task RemoveCommentAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public async Task<IEnumerable<CommentResponse>> GetCommentsByGameIdAsync(int id)
+    {
+        var comments = await _commentRepository.GetAllByGameIdAsync(id);
+
+        var commentResponses = comments.Select(CommentResponse.FromComment).ToList();
+
+        return commentResponses;
+    }
+}
---
+++ Gamestore/Gamestore.Api/Services/Interfaces/ICommentService.cs
@@ -0,0 +1,40 @@
+﻿using Gamestore.Api.Models.DTO.CommentDTO;
+using Gamestore.Database.Entities;
+
+namespace Gamestore.Api.Services.Interfaces;
+
+public interface ICommentService
+{
+    /// <summary>
+    /// Adds a new comment.
+    /// </summary>
+    /// <param name="commentRequest">The comment to add.</param>
+    /// <param name="game">The game to which the comment will be added.</param>
+    Task AddCommentAsync(CommentRequest commentRequest, Game game);
+
+    /// <summary>
+    /// Retrieves a comment by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the comment to retrieve.</param>
+    /// <returns>The retrieved comment.</returns>
+    Task<CommentResponse?> GetCommentByIdAsync(int id);
+
+    /// <summary>
+    /// Updates an existing comment.
+    /// </summary>
+    /// <param name="commentRequest">The comment to update.</param>
+    Task UpdateCommentAsync(CommentRequest commentRequest);
+
+    /// <summary>
+    /// Removes a comment by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the comment to remove.</param>
+    Task RemoveCommentAsync(int id);
+
+    /// <summary>
+    /// Retrieves all comments associated with a specific game.
+    /// </summary>
+    /// <param name="id">The ID of game.</param>
+    /// <returns>A collection of comments associated with the game.</returns>
+    Task<IEnumerable<CommentResponse>> GetCommentsByGameIdAsync(int id);
+}
