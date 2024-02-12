1ee9d98623637fad720d1f59ece9a9d9058e5fc5
Illia Semenenko <illia_semenenko@epam.com>
2024-02-13 01:10:34 +0200

    Added CommentRepository

---
+++ Gamestore/Gamestore.DataBase/Repositories/CommentRepository.cs
@@ -0,0 +1,83 @@
+﻿using Gamestore.Database.Dbcontext;
+using Gamestore.Database.Entities;
+using Gamestore.Database.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gamestore.Database.Repositories;
+
+/// <summary>
+/// Provides methods for managing comments in the database.
+/// Implements the <see cref="ICommentRepository"/> interface.
+/// </summary>
+public class CommentRepository : ICommentRepository
+{
+    private readonly GamestoreContext _context;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CommentRepository"/> class.
+    /// </summary>
+    /// <param name="context">The database context.</param>
+    public CommentRepository(GamestoreContext context)
+    {
+        _context = context;
+    }
+
+    /// <inheritdoc />
+    public async Task<Comment?> GetByIdAsync(int id)
+    {
+        return await _context.Comments.FindAsync(id);
+    }
+
+    /// <inheritdoc />
+    public async Task<IEnumerable<Comment>> GetAllByGameIdAsync(int id)
+    {
+        return await _context.Comments
+            .Where(comment => comment.Game.Id == id && comment.ParentId == null)
+            .Include(comment => comment.ChildComments)
+                .ThenInclude(childComment => childComment.ChildComments)
+            .ToListAsync();
+    }
+
+    /// <inheritdoc />
+    public async Task AddAsync(Comment comment)
+    {
+        _context.Comments.Add(comment);
+        await SaveChangesAsync("Error when adding the comment to the database.");
+    }
+
+    /// <inheritdoc />
+    public async Task RemoveAsync(int id)
+    {
+        // TODO cascade remove
+        var comment = await _context.Comments.SingleOrDefaultAsync(g => g.Id.Equals(id))
+                   ?? throw new ArgumentException($"No comment found with the ID '{id}'.", nameof(id));
+
+        _context.Comments.Remove(comment);
+
+        await SaveChangesAsync("Error when deleting the game from the database.");
+    }
+
+    /// <inheritdoc />
+    public async Task UpdateAsync(Comment comment)
+    {
+        _context.Entry(comment).State = EntityState.Modified;
+
+        await SaveChangesAsync("Error when updating the comment in the database.");
+    }
+
+    /// <summary>
+    /// Asynchronously saves changes to the database context and throws a <see cref="DbUpdateException"/>
+    /// with the specified error message if no changes were saved.
+    /// </summary>
+    /// <param name="errorMessage">The error message to be included in the exception if no changes were saved.</param>
+    /// <returns>An asynchronous task representing the operation's completion or throwing a <see cref="DbUpdateException"/>.</returns>
+    private async Task SaveChangesAsync(string errorMessage)
+    {
+        var saved = await _context.SaveChangesAsync();
+
+        if (saved == 0)
+        {
+            throw new DbUpdateException(errorMessage);
+        }
+    }
+}
---
+++ Gamestore/Gamestore.DataBase/Repositories/Interfaces/ICommentRepository.cs
@@ -0,0 +1,42 @@
+﻿using Gamestore.Database.Entities;
+
+namespace Gamestore.Database.Repositories.Interfaces;
+
+/// <summary>
+/// Defines the operations for managing comments in the database.
+/// </summary>
+public interface ICommentRepository
+{
+    /// <summary>
+    /// Creates a new comment in the database.
+    /// </summary>
+    /// <param name="comment">The comment to create.</param>
+    Task AddAsync(Comment comment);
+
+    /// <summary>
+    /// Retrieves a comment from the database by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the comment to retrieve.</param>
+    /// <returns>The retrieved comment.</returns>
+    Task<Comment?> GetByIdAsync(int id);
+
+    /// <summary>
+    /// Updates an existing comment in the database.
+    /// </summary>
+    /// <param name="comment">The comment to update.</param>
+    /// <returns>The updated comment.</returns>
+    Task UpdateAsync(Comment comment);
+
+    /// <summary>
+    /// Deletes a comment from the database by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the comment to delete.</param>
+    Task RemoveAsync(int id);
+
+    /// <summary>
+    /// Retrieves all comments associated with a specific game from the database.
+    /// </summary>
+    /// <param name="id">The ID of the comments to retrieve.</param>
+    /// <returns>A collection of comments associated with the game.</returns>
+    Task<IEnumerable<Comment>> GetAllByGameIdAsync(int id);
+}
